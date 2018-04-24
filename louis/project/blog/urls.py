ac786607487799c502addb831e9f2a3c7dc88a49
dagusen <louisaleksieb.dagusen@gmail.com>
2018-04-25 07:39:56 +0800

    Add login

--- louis/project/blog/urls.py
+++ louis/project/blog/urls.py
@@ -8,7 +8,7 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', PostListView.as_view(), name='post-list'),
-    path('<slug:slug>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/<slug:slug>/', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<slug:slug>/edit/', views.post_edit, name='post-edit'),
     # path('post/<int:post_id>/', PostView.as_view(), name='post-list'),
--- louis/project/project/urls.py
+++ louis/project/project/urls.py
@@ -20,11 +20,19 @@ from django.conf.urls.static import static
 
 from django.contrib import admin
 
-from django.urls import path, include
+from django.urls import path, include, reverse_lazy
+
+from django.contrib.auth.views import(
+    LoginView,
+    LogoutView,
+    )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls', namespace='blog'))
+    path('', include('blog.urls', namespace='blog')),
+    path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page=reverse_lazy('login')), name='logout'),
 ]
 
 if settings.DEBUG:
