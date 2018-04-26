a996c85c57597be23ca45ff285b807e6dc76bbe0
dagusen <louisaleksieb.dagusen@gmail.com>
2018-04-26 10:12:16 +0800

    Update comment migrate

--- louis/project/blog/models.py
+++ louis/project/blog/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-from django.db.models.signals import pre_save
+from django.db.models.signals import pre_save, post_save
 
 from django.conf import settings
 
@@ -94,7 +94,7 @@ class Comment(models.Model):
         return '{}'.format(self.text)
 
 class Profile(models.Model):
-    user                    = models.ForeignKey(User, on_delete=models.CASCADE)
+    user                    = models.OneToOneField(User, on_delete=models.CASCADE)
     picture                 = models.ImageField(upload_to='media')
     first_name              = models.CharField(max_length=255)
     middle_name             = models.CharField(max_length=255)
@@ -103,6 +103,9 @@ class Profile(models.Model):
     def __str__(self):
         return '{} {} {}'.format(self.first_name, self.middle_name, self.last_name)
 
+def post_save_user_receiver(sender, instance, created, *args, **kwargs):
+    if created:
+        proflie, is_created = Profile.objects.get_or_create(user=instance)
 
 def rl_pre_save_receiver(sender, instance, *args, **kwargs):
     if not instance.slug:
@@ -112,3 +115,4 @@ pre_save.connect(rl_pre_save_receiver, sender=Post)
 pre_save.connect(rl_pre_save_receiver, sender=Category)
 pre_save.connect(rl_pre_save_receiver, sender=Tag)
 pre_save.connect(rl_pre_save_receiver, sender=Comment)
+post_save.connect(post_save_user_receiver, sender=User)
